686d99b679dde74b95132ba51df433362b6998c3
saprameya <156042083+saprameya@users.noreply.github.com>
2024-03-27 14:56:22 -0400

    Add files via upload

--- sd_script.js
+++ sd_script.js
@@ -5,8 +5,8 @@ var itemImg = new Array("chocolate chip cookies on a plate.png","double chocolat
 var itemAltTxt = new Array("chocolate chip cookies on a plate", "double chocolate chip cookies on a plate", "brownies on a plate");
 var itemId = new Array("choc-chip", "dbl-choc-chip", "brownies");
 var itemTitle = new Array("Classic Chocolate<br>Chip Cookies", "Double Chocolate<br>Chip Cookies", "Brownies");
-var itemPrice = new Array("$2.85", "$2.85", "$4.25");
-var buttonNum;
+var itemPrice = new Array(2.85, 2.85, 4.25);
+var itemButton;
 
 var item = document.getElementById("menu-main");
 item.innerHTML = "";
@@ -14,8 +14,8 @@ for(var i = 0; i<itemCardId.length; i++){
     item.innerHTML += '<article class="item-card" id="' + itemCardId[i] + '">'+
     '<img src="' + itemImg[i] + '" alt="'+ itemAltTxt[i] + '" class = "item-img" id = "' + itemId[i] + '">' +
     '<h3 class="item-title" id="title">' + itemTitle[i] + '</h3> ' +
-    '<div class="price">' + itemPrice[i] + '</div>' + 
-    '<button type="submit" class="order-button" id="' + (i+1) + '">Add to cart</button>';
+    '<div class="price">'+'$' + itemPrice[i] + '</div>' + 
+    '<button onclick ="addToCart()" type="submit" class="order-button" id="' + (i+1) + '">Add to cart</button>';
 }
 
 item.innerHTML += '</article>'
